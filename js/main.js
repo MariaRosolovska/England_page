a5ef56a4ce7d5ae1cd888db4d0b5cc278255f127
Maria Rosolovska <feofanmaria19@gmail.com>
2022-03-12 20:24:04 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    $('.photo__slider').slick({
+        
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/button/Arrow-left.png" alt="" style="width: 40px; height: 40px;"></button>',
+        nextArrow: '<button type="button" class="slick-next">  <img src="images/button/Arrow-right.png" alt="" style="width: 40px; height: 40px;"></button>'
+    });
+
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
+});
